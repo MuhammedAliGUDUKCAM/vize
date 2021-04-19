f204e2ae45566f3633d4cc8f54577d3b2686ea33
Muhammed Ali GÜDÜKÇAM <m.ali_gdkcm@hotmail.com>
2021-04-19 23:29:53 +0300

    proje oldu

--- vize/Program.cs
+++ vize/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
 
 namespace vize
 {
@@ -10,6 +13,37 @@ namespace vize
     {
         static void Main(string[] args)
         {
+            XmlDocument ahaberXml = new XmlDocument();
+            XmlDocument XmlBenim = new XmlDocument();
+
+            XmlBenim.Load("ahaber.xml");
+
+            XElement root = XElement.Load("ahaber.xml");
+
+            ahaberXml.Load("https://www.ahaber.com.tr/rss/anasayfa.xml");
+
+
+            root.RemoveAll();
+
+            foreach (XmlNode item in ahaberXml.SelectNodes("rss/channel/item"))
+            {
+                Console.WriteLine(item["title"].InnerText);
+
+                XElement medya = new XElement("medya");
+                XElement title = new XElement("title");
+
+                title.Value = item["title"].InnerText;
+
+                medya.Add(title);
+                root.Add(medya);
+
+            }
+
+
+            root.Save("ahaber.xml");
+
+            Console.ReadKey();
+
         }
     }
 }
